d917ca064a20231dba9192fe973fbd8781586036
Pawel Alessandro Reynoso Marquez <pawelreynoso@gmail.com>
2022-02-07 09:55:16 -0600

    Se agregó comboBox y WebBrowser

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.irAdelanteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.buttonIr = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +46,7 @@
             this.navegarToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1232, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1904, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -88,18 +90,43 @@
             // 
             this.buttonIr.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.buttonIr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonIr.Location = new System.Drawing.Point(985, 44);
+            this.buttonIr.Location = new System.Drawing.Point(883, 44);
             this.buttonIr.Name = "buttonIr";
-            this.buttonIr.Size = new System.Drawing.Size(63, 39);
+            this.buttonIr.Size = new System.Drawing.Size(70, 33);
             this.buttonIr.TabIndex = 1;
             this.buttonIr.Text = "Ir";
             this.buttonIr.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "https://www.google.com",
+            "https://www.mesoamericana.edu.gt",
+            "https://www.facebook.com",
+            "https://www.youtube.com",
+            "https://twitter.com"});
+            this.comboBox1.Location = new System.Drawing.Point(200, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(668, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(23, 143);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1600, 760);
+            this.webBrowser1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1232, 817);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonIr);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -122,6 +149,8 @@
         private System.Windows.Forms.ToolStripMenuItem irAdelanteToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
         private System.Windows.Forms.Button buttonIr;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
