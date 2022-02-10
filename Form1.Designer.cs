0be80d89184a073566d57313c7f4b6f26ff59d0c
Pawel Alessandro Reynoso Marquez <pawelreynoso@gmail.com>
2022-02-10 16:45:06 -0600

    haciendo pruebas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.buttonIr = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -67,7 +68,7 @@
             // 
             this.inicioToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("inicioToolStripMenuItem.Image")));
             this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
             this.inicioToolStripMenuItem.Text = "Inicio";
             this.inicioToolStripMenuItem.Click += new System.EventHandler(this.inicioToolStripMenuItem_Click);
             // 
@@ -75,7 +76,7 @@
             // 
             this.irAtrásToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("irAtrásToolStripMenuItem.Image")));
             this.irAtrásToolStripMenuItem.Name = "irAtrásToolStripMenuItem";
-            this.irAtrásToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.irAtrásToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
             this.irAtrásToolStripMenuItem.Text = "Ir Atrás";
             this.irAtrásToolStripMenuItem.Click += new System.EventHandler(this.irAtrásToolStripMenuItem_Click);
             // 
@@ -83,7 +84,7 @@
             // 
             this.irAdelanteToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("irAdelanteToolStripMenuItem.Image")));
             this.irAdelanteToolStripMenuItem.Name = "irAdelanteToolStripMenuItem";
-            this.irAdelanteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.irAdelanteToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
             this.irAdelanteToolStripMenuItem.Text = "Ir Adelante";
             this.irAdelanteToolStripMenuItem.Click += new System.EventHandler(this.irAdelanteToolStripMenuItem_Click);
             // 
@@ -91,7 +92,7 @@
             // 
             this.salirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("salirToolStripMenuItem.Image")));
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
             this.salirToolStripMenuItem.Text = "Salir";
             this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
@@ -112,10 +113,10 @@
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "https://www.google.com",
             "https://www.youtube.com",
             "https://facebook.com",
-            "https://mesoamericana.edu.gt"});
+            "https://mesoamericana.edu.gt",
+            "https://www.google.com"});
             this.comboBox1.Location = new System.Drawing.Point(200, 44);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(668, 28);
@@ -129,11 +130,20 @@
             this.webBrowser1.Size = new System.Drawing.Size(1184, 542);
             this.webBrowser1.TabIndex = 3;
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(247, 113);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(333, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 731);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonIr);
@@ -161,6 +171,7 @@
         private System.Windows.Forms.Button buttonIr;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
